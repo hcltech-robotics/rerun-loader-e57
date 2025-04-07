5601ded9c653db75e4c65a9cc27a702fef34e98e
Istvan Fodor <586159+istvan-fodor@users.noreply.github.com>
2025-04-07 17:05:45 -0500

    Feat/scan params (#1)
    * feat: limit displayed scans with RERUN_E57_DISPLAY_SCANS env var
    
    * feat: markers for scan locations derived from translate
    
    * fix: fixed branch on rust build
    
    * fix: fixed cargo clippy errors

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::collections::HashSet;
+use std::env;
 use anyhow::{Context, Result};
 use e57::{CartesianCoordinate, E57Reader};
-use rerun::{EXTERNAL_DATA_LOADER_INCOMPATIBLE_EXIT_CODE};
+use rerun::{Points3D, EXTERNAL_DATA_LOADER_INCOMPATIBLE_EXIT_CODE};
 use rerun::{RecordingStreamBuilder, Vec3D};
+
 /// Command line arguments for the E57 Rerun data loader.
 #[derive(argh::FromArgs, Debug)]
 #[argh(description = "Load E57 point clouds and stream them to Rerun")]
@@ -29,18 +32,21 @@ struct Args {
         switch,
         description = "optionally mark data to be logged statically"
     )]
+    #[allow(dead_code)]
     static_: bool,
 
     #[argh(
         option,
         description = "optional timestamps to log at (e.g. --time sim_time=1709203426)"
     )]
+    #[allow(dead_code)]
     time: Vec<String>,
 
     #[argh(
         option,
         description = "optional sequences to log at (e.g. --sequence sim_frame=42)"
     )]
+    #[allow(dead_code)]
     sequence: Vec<String>,
 }
 
@@ -50,6 +56,21 @@ fn extension(path: &std::path::Path) -> String {
         .to_ascii_lowercase()
         .to_string_lossy()
         .to_string()
+}
+
+fn get_allowed_scans() -> Option<HashSet<usize>> {
+    let allowed_scans: Option<HashSet<usize>> = match env::var("RERUN_E57_DISPLAY_SCANS") {
+        Ok(val) => {
+            let set = val
+                .split(',')
+                .filter_map(|s| s.trim().parse::<usize>().ok())
+                .collect::<HashSet<_>>();
+            Some(set)
+        }
+        Err(_) => None,
+    };
+    
+    allowed_scans
 }
 
 fn main() -> Result<()> {
@@ -87,7 +108,11 @@ fn main() -> Result<()> {
         rec.stdout()?
     };
 
-    rec.set_timepoint(timepoint_from_args(&args)?);
+    // if let Some(timepoint) = timepoint_from_args(&args) {
+    //     rec.set_timepoint(timepoint);
+    // }
+
+    let allowed_scans = get_allowed_scans();
 
     let entity_path_prefix = args
         .entity_path_prefix
@@ -104,6 +129,12 @@ fn main() -> Result<()> {
         if pointcloud.records < 1 {
             println!("Point cloud #{index} is empty, skipping...");
             continue;
+        }
+
+        if let Some(allowed_scans) = &allowed_scans {
+            if !allowed_scans.contains(&index) {
+                continue;
+            }
         }
 
         let iter = reader
@@ -139,6 +170,24 @@ fn main() -> Result<()> {
         //     )?;
         // }
 
+        rec.set_time_seconds("default", 0);
+        if let Some(transform) = &pointcloud.transform {
+            let translation = &transform.translation;
+
+            let translation = [(
+                translation.x as f32,
+                translation.y as f32,
+                translation.z as f32,
+            )];
+            rec.log(
+                format!("{entity_path_prefix}/scan_{index}/point"),
+                &Points3D::new(translation)
+                    .with_colors([rerun::Color::from_rgb(255, 0, 0)])
+                    .with_radii([0.15_f32])
+                    .with_labels([format!("Scan {index}")])
+            )?;
+        }
+
         for point_result in iter {
             let p = match point_result {
                 Ok(p) => p,
@@ -148,29 +197,26 @@ fn main() -> Result<()> {
                 }
             };
 
-            match p.cartesian {
-                CartesianCoordinate::Valid { x, y, z } => {
-                    buffer.push(Vec3D::new(x as f32, y as f32, z as f32));
-                    let color = match p.color {
-                        Some(color) => rerun::Color::from_rgb(
-                            (color.red * 255.0) as u8,
-                            (color.green * 255.0) as u8,
-                            (color.blue * 255.0) as u8,
-                        ),
-                        _ => rerun::Color::from_rgb(255, 255, 255),
-                    };
-                    color_buffer.push(color)
-                }
-                _ => {}
+            if let CartesianCoordinate::Valid { x, y, z } = p.cartesian {
+                buffer.push(Vec3D::new(x as f32, y as f32, z as f32));
+                let color = match p.color {
+                    Some(color) => rerun::Color::from_rgb(
+                        (color.red * 255.0) as u8,
+                        (color.green * 255.0) as u8,
+                        (color.blue * 255.0) as u8,
+                    ),
+                    _ => rerun::Color::from_rgb(255, 255, 255),
+                };
+                color_buffer.push(color)
             }
 
             if buffer.len() >= chunk_size {
-                rec.set_time_seconds("default", 0);
                 rec.log(
                     format!("{entity_path_prefix}/scan_{index}/chunk_{chunk_idx}"),
                     &rerun::Points3D::new(std::mem::take(&mut buffer))
                         .with_colors(color_buffer.clone()),
                 )?;
+
                 buffer.clear();
                 color_buffer.clear();
                 chunk_idx += 1;
@@ -188,26 +234,50 @@ fn main() -> Result<()> {
     Ok(())
 }
 
-fn timepoint_from_args(args: &Args) -> anyhow::Result<rerun::TimePoint> {
-    let mut timepoint = rerun::TimePoint::default();
+// fn timepoint_from_args(args: &Args) -> Option<rerun::TimePoint> {
+//     if args.time.is_empty() && args.sequence.is_empty() {
+//         return None;
+//     }
 
-    for time_str in &args.time {
-        if let Some((timeline_name, time)) = time_str.split_once('=') {
-            timepoint.insert(
-                rerun::Timeline::new_temporal(timeline_name),
-                time.parse::<i64>()?,
-            );
-        }
-    }
+//     let mut timepoint = rerun::TimePoint::default();
 
-    for seq_str in &args.sequence {
-        if let Some((seqline_name, seq)) = seq_str.split_once('=') {
-            timepoint.insert(
-                rerun::Timeline::new_sequence(seqline_name),
-                seq.parse::<i64>()?,
-            );
-        }
-    }
+//     for time_str in &args.time {
+//         if let Some((timeline_name, time)) = time_str.split_once('=') {
 
-    Ok(timepoint)
-}
+//             let parsed_time = match time.parse::<i64>() {
+//                 Ok(parsed_time) => parsed_time,
+//                 Err(_) => {
+//                     eprintln!("Invalid time value: {time}");
+//                     return None
+//                 }
+//             };
+
+//             timepoint.insert(
+//                 rerun::Timeline::new_temporal(timeline_name),
+//                 parsed_time,
+//             );
+//         }
+//     }
+
+//     for seq_str in &args.sequence {
+
+        
+//         if let Some((seqline_name, seq)) = seq_str.split_once('=') {
+
+//             let parsed_time = match seq.parse::<i64>() {
+//                 Ok(parsed_time) => parsed_time,
+//                 Err(_) => {
+//                     eprintln!("Invalid time value: {seq}");
+//                     return None
+//                 }
+//             };
+            
+//             timepoint.insert(
+//                 rerun::Timeline::new_sequence(seqline_name),
+//                 parsed_time,
+//             );
+//         }
+//     }
+
+//     Some(timepoint)
+// }
